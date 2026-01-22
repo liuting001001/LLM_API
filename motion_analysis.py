741e9ac83589f219647fe31bd172df7241ffaea5
2201_76050230 <2201_76050230@noreply.gitcode.com>
2026-01-22 15:11:31 +0800

    add actual implementation code

---
+++ motion_analysis.py
@@ -0,0 +1,27 @@
+import json
+import os
+import dashscope
+from dashscope.api_entities.dashscope_response import Role
+import dotenv
+dotenv.load_dotenv()
+dashscope.api_key = os.getenv("DASHSCOPE_API_KEY")
+
+
+# 封装模型响应函数
+def get_response(messages):
+    response = dashscope.Generation.call(
+        model='qwen-turbo-latest',
+        messages=messages,
+        result_format='message'  # 将输出设置为message形式
+    )
+    return response
+
+
+review = '这款音效特别拉跨 给你意想不到的音质。'
+messages = [
+    {"role": "system", "content": "你是一名舆情分析师，帮我判断产品口碑的正负向，回复请用一个词语：正向 或者 负向"},
+    {"role": "user", "content": review}
+]
+
+response = get_response(messages)
+print(response.output.choices[0].message.content)
